3c9579311007e38e91ecf04c318dab720f66a033
RK <rkvishwakarma2050@gmail.com>
2018-09-06 15:27:23 +0530

    rk

--- app/src/main/java/com/hvantage/medicineapp/fragments/UploadPrecriptionFragment.java
+++ app/src/main/java/com/hvantage/medicineapp/fragments/UploadPrecriptionFragment.java
@@ -3,42 +3,21 @@ package com.hvantage.medicineapp.fragments;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
-import android.util.Base64;
-import android.util.Log;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.hvantage.medicineapp.BuildConfig;
 import com.hvantage.medicineapp.R;
-import com.hvantage.medicineapp.model.PrescriptionModel;
 import com.hvantage.medicineapp.util.FragmentIntraction;
 import com.hvantage.medicineapp.util.ProgressBar;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-
-import static android.app.Activity.RESULT_OK;
 
 
 public class UploadPrecriptionFragment extends Fragment implements View.OnClickListener {
@@ -55,7 +34,7 @@ public class UploadPrecriptionFragment extends Fragment implements View.OnClickL
     private ProgressBar progressBar;
     private String userChoosenTask;
     private double total = 0;
-    private TextView tvInstructions;
+    private AppCompatTextView tvInstructions;
 
     @SuppressLint("LongLogTag")
     @Nullable
@@ -97,28 +76,20 @@ public class UploadPrecriptionFragment extends Fragment implements View.OnClickL
                     ft.replace(R.id.main_container, fragment);
                     ft.addToBackStack(null);
                     ft.commitAllowingStateLoss();
-                } /*else if (items[item].equals("Camera")) {
-                    userChoosenTask = "Camera";
-                    cameraIntent();
-                } else if (items[item].equals("Gallery")) {
-                    userChoosenTask = "Gallery";
-                    galleryIntent();
-                }*/
+                }
             }
         });
-//      builder.setCancelable(false);
         builder.show();
     }
-
 
     private void init() {
         btnUpload = (RelativeLayout) rootView.findViewById(R.id.btnUpload);
         btnChoose = (RelativeLayout) rootView.findViewById(R.id.btnChoose);
-        tvInstructions = (TextView) rootView.findViewById(R.id.tvInstructions);
+        tvInstructions = (AppCompatTextView) rootView.findViewById(R.id.tvInstructions);
         btnUpload.setOnClickListener(this);
         btnChoose.setOnClickListener(this);
+        tvInstructions.setOnClickListener(this);
     }
-
 
     @Override
     public void onAttach(Context context) {
@@ -162,195 +133,18 @@ public class UploadPrecriptionFragment extends Fragment implements View.OnClickL
                 ft.addToBackStack(null);
                 ft.commitAllowingStateLoss();
                 break;
+            case R.id.tvInstructions:
+                dialogPrescGuide();
+                break;
         }
     }
 
-    private void showNoPresAlert() {
-        new AlertDialog.Builder(context)
-                .setMessage("Please upload atleast one presciptions to order with prescription.")
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //selectImage();
-                        askAlert();
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .show();
+    private void dialogPrescGuide() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_presc_guide, null);
+        dialog.setView(dialogView);
+        final AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
     }
-
-    private void showProgressDialog() {
-        progressBar = ProgressBar.show(context, "Processing...", true, false, new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                // TODO Auto-generated method stub
-            }
-        });
-    }
-
-    private void hideProgressDialog() {
-        if (progressBar != null)
-            progressBar.dismiss();
-    }
-
-    private void selectImage() {
-        final CharSequence[] items = {"Camera", "Gallery"};
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
-                if (items[item].equals("Camera")) {
-                    userChoosenTask = "Camera";
-                    cameraIntent();
-                } else if (items[item].equals("Gallery")) {
-                    userChoosenTask = "Gallery";
-                    galleryIntent();
-                }
-            }
-        });
-        builder.show();
-    }
-
-    @Nullable
-    private Intent createPickIntent() {
-        Intent picImageIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        if (picImageIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            return picImageIntent;
-        } else {
-            return null;
-        }
-    }
-
-    private void cameraIntent() {
-        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/medicineapp/";
-        File newdir = new File(dir);
-        newdir.mkdirs();
-        String file = dir + "prescription_img.jpg";
-        Log.e("imagesss cam11", file);
-        File newfile = new File(file);
-        try {
-            newfile.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        final Uri outputFileUri;
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-            outputFileUri = FileProvider.getUriForFile(context,
-                    BuildConfig.APPLICATION_ID + ".provider", newfile);
-        } else {
-            outputFileUri = Uri.fromFile(newfile);
-        }
-        Log.e(TAG, "cameraIntent: outputFileUri >> " + outputFileUri);
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-    }
-
-    private void galleryIntent() {
-        startActivityForResult(createPickIntent(), REQUEST_LOAD_IMAGE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == REQUEST_LOAD_IMAGE && data != null) {
-                startCropImageActivity(data.getData());
-            } else if (requestCode == REQUEST_IMAGE_CAPTURE) {
-                File croppedImageFile1 = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                        + "/medicineapp/" + "prescription_img.jpg");
-                final Uri outputFileUri;
-                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-                    outputFileUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", croppedImageFile1);
-                } else {
-                    outputFileUri = Uri.fromFile(croppedImageFile1);
-                }
-                Log.e(TAG, " Inside REQUEST_IMAGE_CAPTURE uri :- " + outputFileUri);
-                startCropImageActivity(outputFileUri);
-            } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-                CropImage.ActivityResult result = CropImage.getActivityResult(data);
-                if (resultCode == RESULT_OK) {
-                    Bitmap bitmap = null;
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), result.getUri());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    new ImageTask().execute(bitmap);
-                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                    Toast.makeText(getActivity(), "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-    }
-
-    private void startCropImageActivity(Uri imageUri) {
-        CropImage.activity(imageUri)
-                .setGuidelines(CropImageView.Guidelines.ON)
-                .setMultiTouchEnabled(false)
-                .setAspectRatio(3, 4)
-                .setOutputCompressQuality(50)
-//                .setRequestedSize(200, 200)
-                .setScaleType(CropImageView.ScaleType.CENTER_INSIDE)
-                .start(getContext(), this);
-    }
-
-    class ImageTask extends AsyncTask<Bitmap, String, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            showProgressDialog();
-        }
-
-        @Override
-        protected Void doInBackground(Bitmap... bitmaps) {
-            Bitmap bitmapImage = bitmaps[0];
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
-            byte[] byteArray = byteArrayOutputStream.toByteArray();
-            String Encoded_userimage = Base64.encodeToString(byteArray, Base64.DEFAULT);
-            publishProgress(Encoded_userimage);
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values);
-            String image_base64 = values[0];
-            final PrescriptionModel data = new PrescriptionModel("", image_base64);
-            Log.d(TAG, "onProgressUpdate: image_base64 >> " + image_base64);
-//            presList.add(data);
-            hideProgressDialog();
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            /*  builder.setMessage("Do you want to add prescription medicines")
-                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            startActivity(
-                                    new Intent(context, PrescPreviewActivity.class)
-                                            .putExtra("position", presList.size() - 1));
-                        }
-                    })
-                    .setNegativeButton("Submit Anyway", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-
-                        }
-                    });*/
-
-
-            builder.show();
-
-
-        }
-    }
-
-
 }
